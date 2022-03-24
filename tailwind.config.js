cb808fc719a60ebf15cabc30bb77ffd8baafa750
n0th1ng999 <tiagogabrielpereira@gmail.com>
2022-03-24 01:28:58 +0000

    info bar inicio  . . .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'sunset-yellow':'#F9ED69',
+        'sunset-orange':'#F08A5D',
+        'sunset-maroon':'#B83B5E',
+        'sunset-purple':'#6A2C70',
+
+
+      }
+    },
   },
   plugins: [],
 }
